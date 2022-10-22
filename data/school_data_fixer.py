22150f50a02f50b0fde79f833b6741a64360194b
Grace Austen <112711354+Grace-Austen@users.noreply.github.com>
2022-10-22 15:08:47 -0400

    Added original and processed data
    Processing included removing the metadata that was included with the data download and changing columns that contained a single year to a 5 year range ranging from the original year to that year + 5. Also included is some geographical data. There is a .shp file for world countries.

---
+++ data/school_data_fixer.py
@@ -0,0 +1,25 @@
+import glob
+
+outfile_path = 'manyyearsschooling_data_only_fixed.csv'
+
+path = 'meanyearsschooling_data_only.csv'
+
+count = 0
+
+fout = open(outfile_path, 'w')
+
+fin = open(path, 'r')
+
+print(fin)
+
+Lines = fin.readlines()
+
+for line in Lines:
+    newline = line.split(',')
+    newline[1] = newline[1] + '-'+str(int(newline[1])+5)
+    fout.write(','.join(newline))
+
+fin.close()
+fout.close()
+
+print("success")
