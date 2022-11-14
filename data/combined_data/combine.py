f57c09f0b15f27d676fa533e5978d4ee23fe1831
Spacenin <chepstudios@gmail.com>
2022-11-13 19:31:47 -0500

    combined data

--- data/combined_data/combine.py
+++ data/combined_data/combine.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("combined.csv", "w") as combinedFile:
+with open("combined.csv", "w", newline="") as combinedFile:
     #Hard coded file names
     fileNames = ["fertilityRate_final.csv", "growthratewithmigration_final.csv", "lifeexpectancy_final.csv", "meanagechildbearing_final.csv", 
     "meanyearsdifferencegender25+_final.csv", "meanyearsschooling_final.csv", "migrationmanyages_final.csv", "migrationmanyyears_final.csv"]
@@ -24,7 +24,7 @@ with open("combined.csv", "w") as combinedFile:
         files.append(file)
 
         print(file["data"][0].keys())
-    
+
     #Fields for the combined data
     fields = ["Country", "Continent", "Region", "Period", "Age", "Sex", "FertRate", "MigrRate", "LifeYears", "Mean Age", "YearsDifference", "SchoolYears", "MigrNet"]
 
@@ -32,5 +32,31 @@ with open("combined.csv", "w") as combinedFile:
     writer = csv.DictWriter(combinedFile, fieldnames=fields)
     writer.writeheader()
 
+    for item in files[5]["data"]:
+        dictToWrite = item
+
+        for i in range(8):
+            if i == 5:
+                continue
+
+            for item2 in files[i]["data"]:
+                skip = False
+
+                for key in item2.keys():
+                    if key in dictToWrite:
+                        if item2[key] != dictToWrite[key]:
+                            skip = True
+                            break
+
+                if skip:
+                    continue
+
+                for key in item2.keys():
+                    if key not in dictToWrite:
+                        dictToWrite.update(item2)
+
+        writer.writerow(dictToWrite)
+
+    #Close the csv's
     for i in range(8):
        files[i]["file"].close
